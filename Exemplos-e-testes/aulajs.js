be0f112bfd780d4b84da40dfe06c110c76cc0f58
LuizSptech <luiz.correia@sptech.school>
2026-05-04 14:38:08 -0300

    teste

---
+++ Exemplos-e-testes/aulajs.js
@@ -0,0 +1,22 @@
+/*setTimeout(()=>console.log("oi"),2000);
+
+let hi = setInterval(()=>console.log("hi"),1000)
+
+clearInterval(hi)
+
+
+function somarDoisMaisDois() {
+return 2+2
+}
+function somarDoisMaisTres() {
+return 2+3
+} */
+
+
+
+function somar(a,b) {
+    return a+b 
+}
+
+
+console.log(somar(5,5))
