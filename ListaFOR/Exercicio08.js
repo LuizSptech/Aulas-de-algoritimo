cb13fb8e96f4eb5634823d0582c7c10ee3725a06
LuizSptech <luiz.correia@sptech.school>
2026-05-25 23:00:40 -0300

    exercicios 2For

---
+++ ListaFOR/Exercicio08.js
@@ -0,0 +1,13 @@
+let numeros = [3, 7, 2, 9, 7, 5, 2];
+
+
+for(let i = 0; i < numeros.length; i++){
+    console.log(numeros[i])
+    if(numeros[i] > 5){
+        let big = 0
+        
+    }
+    for(let j = i + j; j < numeros.length; j++){
+
+    }
+}
