30acef22dd0e2a80261f787932acb3f64ddb33a8
LuizSptech <luiz.correia@sptech.school>
2026-06-03 15:09:34 -0300

    matriz

---
+++ matriz.js
@@ -0,0 +1,15 @@
+let matriz = [[1,2,3],[4,5,6],[7,8,9]]
+for (let linha = 0; linha < matriz.length; linha++){
+    for (let coluna = 0; coluna < matriz[linha].length; coluna++){
+
+        console.log(matriz[linha][coluna])
+    }
+}
+
+let matriz3D = [
+    [
+        [
+            1
+        ]
+    ]
+]
