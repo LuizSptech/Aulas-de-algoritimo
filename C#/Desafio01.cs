714938893aaf0e43379aaa18bf01bf82fbcd706f
LuizSptech <luiz.correia@sptech.school>
2026-07-27 00:37:56 -0300

    nova organização

---
+++ C#/Desafio01.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Desafio01 : MonoBehaviour
+{
+
+    public int x = 5;
+    public int y = 7;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int soma = x + y;
+        Debug.Log("a soma de x+y e igual a " + soma );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
