1705dd2b990cb53efa0bd75c53fc3a068dbb96b2
LuizSptech <luiz.correia@sptech.school>
2026-07-28 23:39:36 -0300

    python

---
+++ Python/Variaveis.py
@@ -0,0 +1,32 @@
+# INT
+age = 10;
+lifes = 3;
+
+# FLOAT
+
+
+price = 29.90;
+height = 1.82;
+
+#STRING
+
+name = "luiz";
+city = "São paulo";
+
+#BOOL
+
+online = True;
+offline = False;
+
+#descobrindo tipo de variavel
+
+print(type(name));
+print(type(age));
+print(type(price));
+print(type(offline));
+
+
+# CONCATENAÇÃO
+
+print("Olá " + name);
+print(name, age);
