80d5f69486a4a4364d16898df1ee52051debd628
LuizSptech <luiz.correia@sptech.school>
2026-08-10 15:16:23 -0300

    lista python

---
+++ Java/Desafio.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nome = in.nextLine();
+       System.out.println(nome);
+
+
+    }
+
+
+}
